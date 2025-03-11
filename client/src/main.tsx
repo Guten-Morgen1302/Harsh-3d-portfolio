27827a0533d83e5f8052d75849ceabb143b3fbf0
harshabhaypatil <40375067-harshabhaypatil@users.noreply.replit.com>
2025-03-11 06:50:22 +0000

    Restored to 'd39689e2095080f9e2eaf931e2bc73d48ff7f3b4'
    Replit-Restored-To: d39689e2095080f9e2eaf931e2bc73d48ff7f3b4

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
-import { Toaster } from './components/ui/toaster';
-import { QueryClientProvider } from '@/lib/queryClient';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-          <Toaster />
-        </BrowserRouter>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
